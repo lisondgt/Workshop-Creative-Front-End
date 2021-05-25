dbc5110ff57d3c86ce912987ad300cb9b31cc887
lison.dagostini <lison.dagostini@gmail.com>
2021-05-25 17:38:12 +0200

    responsive + début animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 (function ($) {
 
     function responsiveNav() {
-        $(".toggle-navbar").click(function() {
+        $(".toggle-navbar").click(function () {
             $(this).toggleClass("toggle");
             $('.navbar').toggleClass("open");
             $('body').toggleClass('overflow-hidden');
         });
-        if($(window).width() >= 880) {
+        if ($(window).width() >= 880) {
             $('.toggle-navbar').removeClass("toggle");
             $('.navbar').removeClass("open");
             $('body').removeClass('overflow-hidden');
@@ -21,9 +21,9 @@
             prevArrow: $('.slick-left'),
             nextArrow: $('.slick-right'),
             dots: true,
-            customPaging : function(slider, i) {
+            customPaging: function (slider, i) {
                 var thumb = jQuery(slider.$slides[i]).data();
-                return '<a>0'+(i+1)+'</a>';
+                return '<a>0' + (i + 1) + '</a>';
             },
             responsive: [
                 {
@@ -60,14 +60,42 @@
         });
     }
 
-    $(document).ready(function(){
+    var doAnimations = function() {
+
+        // Calc current offset and get all animatables
+        var offset = $(window).scrollTop() + $(window).height(),
+            $animatables = $('.animation-scroll');
+
+        // Unbind scroll handler if we have no animatables
+        if ($animatables.length == 0) {
+            $(window).off('scroll', doAnimations);
+        }
+
+        // Check all animatables and animate them if necessary
+        $animatables.each(function(i) {
+            var $animatable = $(this);
+            if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+                $animatable.removeClass('animatable').addClass('animated');
+            }
+        });
+
+    };
+
+    // Hook doAnimations on scroll, and trigger a scroll
+    $(window).on('scroll', doAnimations);
+    $(window).trigger('scroll');
+
+    $(document).ready(function () {
         responsiveNav();
         slickSlider();
 
     });
 
-    $( window ).resize(function() {
+    $(window).resize(function () {
         responsiveNav();
+    });
+
+    $(window).scroll(function() {
     });
 
 })(jQuery);
