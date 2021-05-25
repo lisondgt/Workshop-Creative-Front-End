8df0d61e9f1d9417c6494bb0db29accd3208bb42
lison.dagostini <lison.dagostini@gmail.com>
2021-05-25 16:18:19 +0200

    responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,39 @@
             customPaging : function(slider, i) {
                 var thumb = jQuery(slider.$slides[i]).data();
                 return '<a>0'+(i+1)+'</a>';
-            }
+            },
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        infinite: true,
+                        arrows: false,
+                    }
+                },
+                {
+                    breakpoint: 720,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        arrows: false,
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
         });
     }
 
